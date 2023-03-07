3ff76a7009f2c45a8f594462eb56e22a84992569
Brightmon <davisbmarsh@gmail.com>
2023-03-07 14:48:22 -0500

    fizzbuzz pp using map method

--- PracticeProblems/fizzBuzz/fizzBuzz.js
+++ PracticeProblems/fizzBuzz/fizzBuzz.js
@@ -1,5 +1,16 @@
 function fizzBuzz(numbers) {
-
+    return numbers.map(myFunction).join('')
 }
+
+function myFunction(num){
+    if(num % 3 === 0 && num % 5 === 0){
+        return 'fizzbuzz'
+    } else if(num % 3 === 0){
+        return 'fizz'
+    } else if(num % 5 === 0){
+        return 'buzz'
+    }
+}
+
 
 module.exports = fizzBuzz;
